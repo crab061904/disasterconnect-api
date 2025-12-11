61f7436456b254d86c911b95b53029157f2b071e
crab061904 <carlrobert0619@gmail.com>
2025-12-11 23:05:33 +0800

    fixed file

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // --- FIX IS HERE: Changed '*' to '/(.*)' ---
-app.options('/(.*)', cors(corsOptions)); 
+app.options(/.*/, cors(corsOptions));
 // ------------------------------------------
 
 app.use(express.json());
